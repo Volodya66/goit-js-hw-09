e7835a707fa64d02a04166a24cb3005e25c3db21
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2023-09-05 12:41:48 +0300

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,51 @@
+const body = document.querySelector('body');
+const buttonOn = body.querySelector('button[data-start]');
+const buttonOf = body.querySelector('button[data-stop]');
+const attributePresent = buttonOf.hasAttribute('disabled');
+let changeResult = false;
+let intervalId;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+// ?
+function changeBackgroundColor() {
+  return new Promise((resolve,reject) => {
+    if (!changeResult) {
+         console.log("статус при натисканні кнопки СТАРТ",changeResult);
+     intervalId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+      resolve();
+    }, 1000);
+    } else {
+      reject('error, the color changes')
+    }
+   
+  });
+};
+buttonOn.addEventListener('click', randomColor);
+function randomColor() {
+  changeResult = false;
+    changeBackgroundColor()
+        .then(() => {
+            buttonOn.setAttribute('disabled', true);
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+};
+
+buttonOf.addEventListener('click', () => {
+  changeResult = true;
+  clearInterval(intervalId);
+  buttonOn.removeAttribute('disabled');
+  body.style.backgroundColor ="";
+  console.log("статус при натисканні кнопки СТОП",changeResult);
+
+})
+
+// ?
+
+
+
 
